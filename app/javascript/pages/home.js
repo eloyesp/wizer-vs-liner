849698c41e505afe7b6ba7a03a12b28cc09022a3
Eloy Espinaco <eloyesp@gmail.com>
2025-02-06 00:52:23 -0300

    Connect React to the backend

--- app/javascript/pages/home.js
+++ app/javascript/pages/home.js
@@ -1,9 +1,28 @@
 import { Link } from "react-router"
+import { useEffect, useState } from "react"
 
 export default function () {
+  const [isLoading, setLoading] = useState(true)
+  const [sequences, setSequences] = useState([])
+
+  useEffect(() => {
+    fetch("/adns.json")
+      .then((response) => response.json())
+      .then((json) => {
+        setLoading(false)
+        setSequences(json)
+      })
+  }, [])
+
+  const renderedSequences = sequences.map((seq) => (
+    <div key={seq.id}>{seq.sequence}</div>
+  ))
+
   return (
     <>
       <h1 className="text-3xl font-medium">Hello from React!</h1>
+      {isLoading && "LOADING"}
+      {renderedSequences}
       <Link to="/new">Add a new sequence</Link>
     </>
   )
