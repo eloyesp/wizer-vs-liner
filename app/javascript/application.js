535c636050104d68dc5b445d250fd3f74c4bd280
Eloy Espinaco <eloyesp@gmail.com>
2025-02-05 22:16:35 -0300

    Add react and react-dom
    Also make sure that build and dev works.

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,1 +1,11 @@
 // Entry point for the build script in your package.json
+
+import { createRoot } from "react-dom/client"
+
+function Application() {
+  return <h1>Hello from React!</h1>
+}
+
+const domNode = document.getElementById("app")
+const root = createRoot(domNode)
+root.render(<Application />)
