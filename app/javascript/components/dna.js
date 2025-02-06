d5845074115b0b3f5435a39bd900f002f1406c5b
Eloy Espinaco <eloyesp@gmail.com>
2025-02-06 11:19:49 -0300

    Working Home

--- app/javascript/components/dna.js
+++ app/javascript/components/dna.js
@@ -1,9 +1,28 @@
 import { Link } from "react-router"
 
+function Nucleotide({ nucleotide }) {
+  const mutant = /^[A-Z]*$/.test(nucleotide) || null
+
+  return (
+    <li
+      className="data-mutant:bg-emerald-100 data-mutant:text-purple-800 size-8 text-center text-lg leading-8"
+      data-mutant={mutant}
+    >
+      {nucleotide}
+    </li>
+  )
+}
+
 export default function DNA({ record }) {
+  const nucleotides = record.sequence.split("")
+
   return (
     <div className="flex items-center">
-      {record.sequence}
+      <ol className="m-2 grid grid-cols-6 items-center gap-1 border-purple-900 p-1 font-mono uppercase">
+        {nucleotides.map((nucleotide, index) => (
+          <Nucleotide key={index} nucleotide={nucleotide} />
+        ))}
+      </ol>
       <Link
         to={`/edit/${record.id}`}
         className="m-4 ml-auto rounded bg-purple-500 px-4 py-2 font-medium text-white shadow-md hover:bg-purple-700 md:m-6 md:py-4"
